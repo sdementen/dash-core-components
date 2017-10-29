90614719d4313a92ff36a1d59a0531994f8026bd
Sébastien de Menten <sdementen@gmail.com>
2017-10-29 07:11:09 +0100

    explicit generation of classes

--- dash_core_components/__init__.py
+++ dash_core_components/__init__.py
@@ -5,11 +5,6 @@ from .version import __version__
 
 _current_path = _os.path.dirname(_os.path.abspath(__file__))
 
-_components = _dash.development.component_loader.load_components(
-    _os.path.join(_current_path, 'metadata.json'),
-    'dash_core_components'
-)
-
 _this_module = _sys.modules[__name__]
 
 _js_dist = [
@@ -48,8 +43,17 @@ _css_dist = [
     }
 ]
 
+_components = _dash.development.component_loader.load_components(
+    _os.path.join(_current_path, 'metadata.json'),
+    'dash_core_components',
+    _js_dist,
+    _css_dist,
+)
 
-for component in _components:
-    setattr(_this_module, component.__name__, component)
-    setattr(component, '_js_dist', _js_dist)
-    setattr(component, '_css_dist', _css_dist)
+
+from .metadata import *
+
+#for component in _components:
+#    setattr(_this_module, component.__name__, component)
+#    setattr(component, '_js_dist', _js_dist)
+#    setattr(component, '_css_dist', _css_dist)
